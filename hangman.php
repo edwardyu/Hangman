310a076a0b6af101f940cfe658b2c1a9152635bc
Edward Yu <edwardy20@yahoo.com>
2013-04-26 22:36:52 -0500

    version 1.0

--- hangman.php
+++ hangman.php
@@ -8,53 +8,72 @@ while(!feof($handle))
 {
     $words[] = fgets($handle);
 }
-
+$guessed_chars = array();
+$current_guess = array();
 //print_r($words);
 
 //computer chooses a word, user has to guess the word within 7 tries. 
 
 function choose_word($words)
 {
-    return array_rand($words);
+    return trim($words[array_rand($words)]);
 }
 
 function check_guess($char, $word)
 {
-    $word = str_split($word);
-    $afterGuess = '';
-    foreach($word as $value)
+    global $guessed_chars;
+    $wordArray = str_split($word);
+    $guessed_chars[] = $char;
+    global $current_guess;
+    if(strpos($word, $char) === false)
     {
-        if($value == $char)
+       return false;
+    }
+    else
+    {
+        for($i = 0; $i < sizeof($wordArray); $i++)
         {
-            $afterGuess .= "$value";
+            if($wordArray[$i] == $char)
+            {
+                $current_guess[$i] = $char;
+            }
+            else if(!isset($current_guess[$i]))
+            {
+                $current_guess[$i] = '';
+            }
         }
-        else
-            $afterGuess .= "_";
+        
+        return $current_guess;
     }
     
-    if(!in_array($char, $word))
-    {
-        print "You guessed wrong!\n";
-        global $wrong;
-        global $wrong_limit;
-        $wrong++;
-        if($wrong == $wrong_limit)
-        {
-            print "Game over!";
-            exit();
-        }
-        else
-            print "You have guessed $wrong wrong characters.\n";
-    }
-    
-    return $afterGuess;
 }
 
 //main interface
 print "Welcome to Hangman!\n";
-print "The computer is choosing a word...";
+print "The computer is choosing a word...\n\n\n";
 $word = choose_word($words);
 $guess = '';
-print "Guess a character: ";
+while(true)
+{
+    print "Guess a character: ";
+    $guess = trim(fgets(STDIN));
+    if(check_guess($guess, $word) == false)
+    {
+        $wrong++;
+        print "You've made $wrong wrong guesses.\n\n\n";
+        if($wrong == $wrong_limit)
+        {
+            print "Sorry, you lose! The correct word is $word.";
+            exit();
+            
+        }
+    }
+    else 
+    {
+     print_r(check_guess($guess, $word));
+    }
+        
+}
+
 
 ?>
